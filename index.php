241b13ae9c66a009fe9aa088cde84c14bda106a9
aitagueye <gueyeanita2626@gmail.com>
2026-06-30 21:10:40 +0000

    Réponse à la question 1 (Format Procédural) - Initialisation

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+
+$categories = [
+    [
+        "code" => "1001",
+        "nom" => "Céréales",
+        "produits" => [
+            [
+                "nom" => "Riz",
+                "reference" => "RIZ01",
+                "prix" => 15000,
+                "quantite" => 10
+            ],
+            [
+                "nom" => "Maïs",
+                "reference" => "MAIS02",
+                "prix" => 12000,
+                "quantite" => 5
+            ]
+        ]
+    ],
+    [
+        "code" => "1002",
+        "nom" => "Légumes",
+        "produits" => [] 
+    ]
+];
