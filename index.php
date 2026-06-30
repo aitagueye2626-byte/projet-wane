e039b488477c375e168673563a72c053fd69dcbd
aitagueye <gueyeanita2626@gmail.com>
2026-06-30 20:43:44 +0000

    Réponse à la question 3 (Format Interactif) - Variables codeExiste et nomExiste

--- index.php
+++ index.php
@@ -30,7 +30,6 @@ $categories = [
 echo "\n--- Catégories sans produits ---\n";
 
 foreach ($categories as $categorie) {
-    // empty() vérifie si le tableau "produits" est vide
     if (empty($categorie["produits"])) {
         echo "Nom : " . $categorie["nom"] . " (Code : " . $categorie["code"] . ")\n";
     }
@@ -47,6 +46,53 @@ foreach ($categories as $categorie) {
 }
 
 
+echo "\n--- Ajout d'une nouvelle catégorie ---\n";
+
+do {
+    $codeExiste = false; 
+    $code = readline("Saisir le code (4 chiffres) : ");
+
+    if (empty($code) || !ctype_digit($code) || strlen($code) !== 4) {
+        echo "Erreur : Format incorrect (4 chiffres obligatoires).\n";
+        $codeExiste = true; 
+    } else {
+        for ($i = 0; $i < count($categories); $i++) {
+            if ($categories[$i]["code"] === $code) {
+                echo "Erreur : Ce code existe déjà.\n";
+                $codeExiste = true;
+                break;
+            }
+        }
+    }
+} while ($codeExiste); 
+
+
+do {
+    $nomExiste = false; 
+    $nom = readline("Saisir le nom : ");
+
+    if (empty($nom)) {
+        echo "Erreur : Le nom est obligatoire.\n";
+        $nomExiste = true; 
+    } else {
+        for ($i = 0; $i < count($categories); $i++) {
+            if (strtolower($categories[$i]["nom"]) === strtolower($nom)) {
+                echo "Erreur : Ce nom existe déjà.\n";
+                $nomExiste = true;
+                break;
+            }
+        }
+    }
+} while ($nomExiste); 
+
+// Ajout direct au tableau
+$categories[] = [
+    "code" => $code,
+    "nom" => $nom,
+    "produits" => []
+];
+
+echo "Succès : Catégorie '$nom' ajoutée !\n";
 
 
 ?>
