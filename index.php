658bc5359cdbc6f1c01e0b2ce1128c5507335e7f
aitagueye <gueyeanita2626@gmail.com>
2026-06-30 21:57:14 +0000

    Réponse Question 2 (Procedural) - Fonction saisie et validation

--- index.php
+++ index.php
@@ -37,3 +37,57 @@ function afficheCategorieSansProduit(array $categories): void {
 }
 
 afficheCategorieSansProduit($categories);
+
+function champObligatoire(string $value, string $message): bool {
+    if (empty($value)) {
+        echo $message . "\n";
+        return false;
+    }
+    return true;
+}
+
+function rechercheCategorieParCle(array $categories, string $key, string $value): int|bool {
+    for ($i = 0; $i < count($categories); $i++) {
+        if (strtolower($categories[$i][$key]) === strtolower($value)) {
+            return $i; 
+        }
+    } 
+    return false; 
+}
+
+function saisieChampObligatoireEtUnique(array $categories, string $smsSaisie, string $smsError, string $key): string {
+    do {   
+        $value = readline($smsSaisie);
+        $valueIsValid = champObligatoire($value, $smsError);
+        
+        if ($valueIsValid) {     
+            if ($key === "code" && (!ctype_digit($value) || strlen($value) !== 4)) {
+                echo "Erreur : Le code doit contenir exactement 4 chiffres.\n";
+                $valueIsValid = false;
+            } 
+            elseif (rechercheCategorieParCle($categories, $key, $value) !== false) {
+                echo "Erreur : Cette valeur existe déjà...\n";
+                $valueIsValid = false;
+            }
+        }
+    } while (!$valueIsValid);
+    
+    return $value;
+}
+function enregistrerCategorie(): void {
+    global $categories;
+    echo "\n--- Enregistrement d'une nouvelle catégorie ---\n";
+
+    $code = saisieChampObligatoireEtUnique($categories, "Entrez le code (4 chiffres) : ", "Champs obligatoire !", "code");
+    $nom = saisieChampObligatoireEtUnique($categories, "Entrez le nom : ", "Champs obligatoire !", "nom");
+
+    $categories[] = [
+        "code" => $code,
+        "nom" => $nom,
+        "produits" => []
+    ];
+    
+    echo "Succès : La catégorie '$nom' a été enregistrée !\n";
+}
+
+enregistrerCategorie();
